c9547f565fc80bd41665bcb7e468f2a490794f12
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-18 11:02:31 +0300

    add auth model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+﻿const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    CPR: {
+        type: Number,
+        require: true,
+        unique: true,
+        minlength: 9,
+        maxlength: 9
+
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    staff: {
+        type:Boolean,
+        required: true,
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
